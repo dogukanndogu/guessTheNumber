d0015c48ec9f67758a69eb7a2f04d776f6dcffe5
dogukanndogu <45043632+dogukanndogu@users.noreply.github.com>
2019-01-30 18:20:12 +0300

    Add files via upload

---
+++ guessTheNumber.py
@@ -0,0 +1,30 @@
+# Created by Doğukan DOĞU
+# Wednesday, January 30th, 2019
+
+import random
+
+secretNumber = random.randint(0, 20)
+guessChance = 6
+print("Hey! I am thinking of a number between 0 and 20. Can you guess it? You have 6 chances.")
+
+for guessesTaken in range(6):
+    guess = int(input("Guess it :"))
+
+    if guess < secretNumber:
+        print("Your guess is too low.")
+        guessChance -= 1
+        print("Your guess chance is {}".format(guessChance))
+
+    elif guess > secretNumber:
+        print("Your guess is too high.")
+        guessChance -= 1
+        print("Your guess chance is {}".format(guessChance))
+
+    else:
+        break
+
+if guess == secretNumber:
+    print("Good job! You guessed my number in" + str(guessesTaken) + "guesses!")
+    print("My number was {}".format(secretNumber))
+else:
+    print("Nope. The number I was thinking of was " + str(secretNumber))
